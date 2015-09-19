d67e86a30015d9732a32b34a759991e573169edc
k-sudo <anti_psychiatry_r_d_laing@yahoo.co.jp>
2015-09-19 16:33:25 +0900

    improve Perceptron class overall

--- perceptron.py
+++ perceptron.py
@@ -14,30 +14,39 @@ def random_matrix(row, col):
     return [[rnd.uniform(-10, 10) for j in range(col)] for i in range(row)]
 
 class Perceptron:
-    def __init__(self, num_in, num_out, wm=False):
-        self._col = num_in + 1
-        self._row = num_out
-        if wm and len(wm) == self._row and len(wm[0]) == self._col:
-            self._wm = wm
-        else:
-            self._wm = random_matrix(self._row, self._col)
+    def __init__(self, num_in, num_out):
+        self._col  = num_in + 1
+        self._row  = num_out
+        self._wm   = random_matrix(self._row, self._col)
+        self._func = step_function
+        self._eta  = 0.1
 
-    def output(self, in_vec):
-        return step_function(mv_product(self._wm, in_vec+[1]))
+    def weight_matrix(self, wm=False):
+        if wm:
+            self._wm, self._row, self._col = wm, len(wm), len(wm[0])
+        return [[self._wm[i][j] for j in range(self._col)] for i in range(self._row)]
 
-    def train(self, case, eta=0.1):
-        out = self.output(case[0])
-        in_vec = case[0]+[1]
+    def activation_function(self, func=False):
+        self._func = func if func else self._func
+        return self._func.__name__
+
+    def learning_coefficient(self, eta=False):
+        self._eta = eta if eta else self._eta
+        return self._eta
+
+    def output(self, i_vec):
+        return self._func(mv_product(self._wm, i_vec+[1]))
+
+    def train(self, case):
+        i_vec = case[0]+[1]
+        o_vec = self.output(case[0])
         for i in range(self._row):
             for j in range(self._col):
-                self._wm[i][j] += eta * (case[1][i] - out[i]) * in_vec[j]
-        return list(self._wm)
+                self._wm[i][j] += self._eta * (case[1][i] - o_vec[i]) * i_vec[j]
 
-    def batch(self, cases, num_loop=100):
+    def batch(self, cases, num_loop=1000):
         for i in range(num_loop):
             self.train(cases[i%len(cases)])
-        return list(self._wm)
-
 
 if __name__ == "__main__":
     def input_and_output(neural_net, logic_gate):
@@ -46,9 +55,9 @@ if __name__ == "__main__":
 
     def demonstration(logic_gate):
         neural_net = Perceptron(2,1)
-        neural_net.batch(logic_gate, 1000)
+        neural_net.batch(logic_gate)
         input_and_output(neural_net, logic_gate)
-        print neural_net.train(logic_gate[0], 0.0)
+        print neural_net.weight_matrix()
 
 
     and_gate = [([0,0],[0]),([0,1],[0]),([1,0],[0]),([1,1],[1])]
